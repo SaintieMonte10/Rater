de2965abfd5d5271ce60107edb1d528682975be9
Clare Richards <claremonte53gmail.com>
2020-06-10 01:55:52 +0300

    added new urls

--- raterapp/urls.py
+++ raterapp/urls.py
@@ -5,6 +5,12 @@ from django.templatetags.static import static
 
 urlpatterns=[
     url('^$',views.home,name='home'),
+    url(r'^new/project$',views.new_project,name='new-project'),
+    url(r'^profile/$',views.profile,name='profile'),
+    url(r'^new_profile/$',views.new_profile,name = 'new_profile'),
+    url(r'^edit/profile/$',views.profile_edit,name = 'edit_profile'),
+    url(r'^search/',views.search_project, name='search_results'),
+    url(r'^project/review/(\d+)',views.project_review,name='project_review'),
 ]
 
 if settings.DEBUG:
--- raterproject/settings.py
+++ raterproject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'raterapp.apps.RaterappConfig',
     'bootstrap4',
-    'tinymce',
 ]
 
 MIDDLEWARE = [
