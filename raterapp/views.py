0008dc466f466969efd206aa88a8e5f88b4f7180
Clare Richards <claremonte53gmail.com>
2020-06-09 23:39:59 +0300

    uninstalled django

--- raterapp/views.py
+++ raterapp/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 
 # Create your views here.
--- raterproject/urls.py
+++ raterproject/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth import views
+from django.urls import path,include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('raterapp.urls')),
+    path('',include('insta.urls')),
 ]
