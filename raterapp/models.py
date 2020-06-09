be7eb8d4a679c26ada635737757552f17c5069e9
Clare Richards <claremonte53gmail.com>
2020-06-10 00:53:11 +0300

    created model profile

--- raterapp/models.py
+++ raterapp/models.py
@@ -43,3 +43,25 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['-id' ]
+
+
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='prof_pics/',blank=True)
+    prof_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    bio = models.TextField()
+    contact_info = models.CharField(max_length=200,blank=True)
+    profile_Id = models.IntegerField(default=0)
+    all_projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()
