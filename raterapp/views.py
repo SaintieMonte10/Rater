fdce25f31566f51b67eac45b3f31b4a8f421f0a4
Clare Richards <claremonte53gmail.com>
2020-06-10 21:23:48 +0300

    worked on responsiveness

--- raterapp/views.py
+++ raterapp/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render,redirect
-from django.http import Http404, HttpResponse
 from django.contrib.auth.models import User
 from .models import Project,Profile
 from .forms import ProjectForm,ProfileForm,VoteForm
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 # Create your views here.
-
 def home(request):
     all_projects = Project.fetch_all_images()
     return render(request,"main/index.html",{"all_images":all_projects})
-
 
 @login_required(login_url='/accounts/login/')
 def new_project(request):
@@ -26,21 +24,6 @@ def new_project(request):
     else:
         form = ProjectForm()
     return render(request,"main/new_project.html",{"form":form})
-
-
-@login_required(login_url='/accounts/login/')
-def profile_edit(request):
-    current_user = request.user
-    if request.method == 'POST':
-        logged_user = Profile.objects.get(prof_user=request.user)
-        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
-        if form.is_valid():
-            form.save()
-        return redirect('profile')
-    else:
-        form = ProfileForm()
-    return render(request,'profile/edit_profile.html',{'form':form})
-    
 
 @login_required(login_url='/accounts/login/')
 def new_profile(request):
@@ -124,5 +107,4 @@ def project_review(request,project_id):
 
     except Exception as  e:
         raise Http404()
-
     return render(request,'main/project_review.html',{"vote_form":vote_form,"single_project":single_project,"average_score":average_score})
