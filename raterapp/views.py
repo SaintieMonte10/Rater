5c186041061dd3a1d494c7b3ab9733303df4c781
Clare Richards <claremonte53gmail.com>
2020-06-10 11:20:18 +0300

    added necessary views

--- raterapp/views.py
+++ raterapp/views.py
@@ -8,12 +8,10 @@ from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
-
-
 def home(request):
     all_projects = Project.fetch_all_images()
     return render(request,"main/index.html",{"all_images":all_projects})
-    
+
 
 @login_required(login_url='/accounts/login/')
 def new_project(request):
@@ -28,3 +26,28 @@ def new_project(request):
     else:
         form = ProjectForm()
     return render(request,"main/new_project.html",{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request,'profile/edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user = current_user)
+
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile/profile.html',{'profile':prof,'projects':projects})
